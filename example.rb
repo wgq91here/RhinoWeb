db7b0c53bf9fc12619b63de2c68711a93a20abbb
wgq91here <wgq91here@gmail.com>
2014-01-02 14:01:34 +0800

    Example

---
+++ example.rb
@@ -0,0 +1,153 @@
+module Application
+  def main
+    {
+        :name => "Simple Blog System",
+        :title => "My Blog",
+        :layer => "3L",
+        :theme => "bootstrap"
+    }
+  end
+
+  #system admin manage
+  def admin
+    {
+        :ip => "127.0.0.1",
+        :weburl => "/webAdmin",
+        :model => 'member, blog'
+    }
+  end
+
+  auth {
+    #system root Manager init and not include db.
+    root {
+      name "admin"
+      passwd "admin"
+    }
+
+    #member own allow system init
+    #ex model
+    authUser {
+      register true
+      owner true
+      profile true
+      model Rhino::member
+      extfield {
+        googlemail mail, true #must
+      }
+    }
+
+    setting {
+
+    }
+  }
+
+  rule {
+
+  }
+
+  def module
+
+  end
+
+  def event
+
+  end
+
+  def plugin
+    {
+        :modle => [
+            :category => 'moudle/rcategory' #category id category->category_id 1->1 field,rule,etc.
+        ]
+    }
+  end
+
+  def page
+    url 'index' do
+    end
+
+    url 'admin' do
+    end
+
+    blog {
+      action ['list', 'list_category', 'update', 'delete', 'view']
+    }
+
+    album {
+      action ['list', 'list_category', 'update', 'delete', 'view']
+    }
+
+    tag {
+      action moudle/rtags # auto find relation model list view & cduv
+    }
+
+    category {
+      action moudle/category
+    }
+
+  end
+
+  modle {
+    member do
+      {
+          model Rhion/member # add all Rhion redefined member fields
+      friend moudle/friend # add friend ids
+      }
+
+      category do
+        {
+            model plugin/category
+        }
+
+        blog do
+          attrs {
+            title string
+            content text
+            createDate date
+            owner moudle/member #userid
+            category category #use module category
+            tag moudle/rtags #auto create tags->blogs *->* field,rule,etc.
+          }
+          event {
+            beforeSave do
+              time()
+            end
+          }
+          setting {
+            :listNum => [
+                :label => '', :selectFields => '10,20,30,#'
+            ]
+            :showOwn => [
+                :label => 'Show Type',
+                [
+                    [:label => 'show by all', :allow => {true}, :action => '/blog.list~/'],
+                    [:label => 'show by own', :deny => {session.userid != own.userid}, :action => 'blog.*'],
+                    [:label => 'show by firends', :allow => {session.userid in own.firends }, :action => 'blog.list, blog.view']
+          ]
+          ]
+          }
+          }
+        end
+
+        album do
+          attrs {
+            title string
+            createDate date
+            owner moudle/user #userid
+            category plugin/category #use module category
+            pictureUrl moudle/uploadfile #uploadfile
+          }
+          event {
+            beforeSave do
+              time()
+            end
+            afterSave {}
+            beforeInsert {}
+            beforeDelete {}
+          }
+          setting {
+            :albumWidth => [:label => 'album Width', :default => 200]
+          }
+        end
+        }
+
+      end
