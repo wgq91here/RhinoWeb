14ac6b04627a24e5824ea73bc5872f8475981349
Fred Wu <wgq91here@gmail.com>
2014-01-29 11:00:16 +0800

    Update example2.rb
    再想想

--- example2.rb
+++ example2.rb
@@ -8,12 +8,28 @@ RConfig = Rhino::Config do
 end
 
 # Widget contains model forms and etc. (ex: html and javasciprt) 
+# it render by templates
 RWidget = Rhino::Widget do
-
+  :list_book_title {
+    :template # default it's name
+    :javascript '' #or :jqueryUI
+  }
+  :list_relate_book_title {
+    :template 'relate_book_title'
+  }
 end
 
 # View contains one widget and more widgets
+# it reander by layer
+# it can contains itself
 RView = Rhino::View do
+  :list_book_name {
+      :layer '2W'
+      :widgets [
+        '1W'=>'list_book_title',
+        '2W'=>'list_relate_book_title'
+        ]
+    }
   :
 end
 
@@ -32,8 +48,13 @@ Rpage.add do
 
 end
 
+# it explane one page to other page
 RFlow = Rhino::Flow do
   
+end
+
+#
+REvent = Rhino::Event do
 end
 
 RApp = Rhino::Application do 
@@ -41,6 +62,7 @@ RApp = Rhino::Application do
   :widget RWidget
   :container RContainer
   :page RPage
+  :event REvent
   :flow RFlow
 end
 
