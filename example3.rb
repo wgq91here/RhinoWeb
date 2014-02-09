863816798c397f8eaf13c27818b403b099aac317
wgq91here <wgq91here@gmail.com>
2014-02-09 15:57:34 +0800

    .gitigonre is change ,but alredy is commited.

--- example3.rb
+++ example3.rb
@@ -3,3 +3,13 @@ my_system = Rhino::Application.new
 my_system::config {
   db = []
 }
+
+
+my_system::config.add {
+  name = 'My System'
+}
+
+my_system::auth {
+  enable = true,
+      user = {:ext_fields => ['int:age:scope(1..2)']}
+}
