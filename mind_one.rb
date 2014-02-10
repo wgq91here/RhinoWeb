570b1cdc297c8e6ad392d0af809291d9de14b2ac
wgq91here <wgq91here@gmail.com>
2014-02-10 15:45:22 +0800

    Add

--- mind_one.rb
+++ mind_one.rb
@@ -9,26 +9,48 @@ module Rh
     puts value
   end
 
+
   #Config = config
   module RConfig
+
+    def add
+      yield
+    end
+
+  end
+
+  module RPages
+    class Rhino_pages
+
       def add
         yield
       end
+
+      def to_s
+        puts self
+      end
+
+      def layout(layoutName)
+        puts layoutName
+      end
+    end
   end
 
 end
 
 my_system = Rh
-puts my_system
-puts my_system::RConfig
-
 
 my_system::RConfig.add do
   df :blog do
     set :auth, true
-    #:rule = ''
-    layout = '3W'
-    wg_3W = [:nav_blog, :list_blog, nil]
-    wg_3W
+    #layout '3W'
+    #wg_3W  [:nav_blog, :list_blog, nil]
+    #wg_3W
+  end
+end
+
+my_system::RPages.add do
+  df :index do
+    layout '3W'
   end
 end
